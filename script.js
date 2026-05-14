3d9217ecbb0f8f5c743e70adeef667b95fbe6a7b
Clarisaci <clarisa.ci@proton.me>
2026-05-14 02:13:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-window.addEventListener("load", () => {
-  console.log("Welcome to Clarisa Creaciones!");
-});
-
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
-
   if (window.scrollY > 50) {
-    header.style.boxShadow = "0 6px 18px rgba(0,0,0,0.08)";
+    header.style.backgroundColor = "#8c2b2b";
   } else {
-    header.style.boxShadow = "none";
+    header.style.backgroundColor = "#6e1b1b";
   }
 });
